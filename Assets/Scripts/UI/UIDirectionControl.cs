6508de726858197de52f646e911fc9554eb421c7
LAPTOP-DGH23R99\Eloi <eloipagniez@gmail.com>
2017-03-04 14:19:12 +0100

    Initial commit

---
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class UIDirectionControl : MonoBehaviour {
+
+    // This class is used to make sure world space UI
+    // elements such as the health bar face the correct direction.
+    public bool m_UseRelativeRotation = true; // Use relative rotation should be usedfor this gameObject ?
+    private Quaternion m_RelativeRotation; // The local rotation at the start of the scene.
+
+	// Use this for initialization
+	void Start () {
+        m_RelativeRotation = transform.parent.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_UseRelativeRotation)
+            transform.rotation = m_RelativeRotation;
+	}
+}
